82282d0d931da86dcfb80f0d03e0289f544cf175
jumpfunky <thotho@posteo.de>
2015-11-26 14:01:23 +0100

    Save and load settings

--- src/main/java/de/biomedical_imaging/ij/ndef/daemon/ParticleSizerDaemon_.java
+++ src/main/java/de/biomedical_imaging/ij/ndef/daemon/ParticleSizerDaemon_.java
@@ -4,21 +4,47 @@ import fiji.util.gui.GenericDialogPlus;
 import ij.IJ;
 import ij.plugin.PlugIn;
 
-
+/**
+ * 
+ * ParticleSizer Daemon tool for the NanoDefine AutoTEM solution. 
+ * 
+ * @author Thorsten Wagner (wagner@biomedical-imaging.de)
+ *
+ */
 public class ParticleSizerDaemon_ implements PlugIn {
 
+	private String coOpPath = "";
+	
+	private final String PREF_COOPPATH = "ndef.daemon.cooppath";
+	
 	@Override
 	public void run(String arg) {
+		showGUI();
+		
+	}
+	
+	private void showGUI(){
+		loadPreferences();
+		
 		GenericDialogPlus gdp = new GenericDialogPlus("Particle Sizer Daemon");
-		gdp.addFileField("Co-op folder", IJ.getDirectory("home"));
+		gdp.addFileField("Co-op folder", coOpPath);
 		gdp.showDialog();
 		
 		if(gdp.wasCanceled()){
 			return;
 		}
 		
-		String coOpPath = gdp.getNextString();
+		coOpPath = gdp.getNextString();
 		
+		savePreferences();
+	}
+	
+	private void loadPreferences(){
+		coOpPath = ij.Prefs.get(PREF_COOPPATH, IJ.getDirectory("home"));
+	}
+	
+	private void savePreferences(){
+		ij.Prefs.set(PREF_COOPPATH,coOpPath);
 	}
 	
 }
